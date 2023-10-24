a984d324db08a416b4f952edf0b3b3fd0a9c9c86
Nathan <nathan@agence-webup.com>
2023-10-24 20:25:25 +0200

    fix(test): coverage

--- client/src/__tests__/NewBill.js
+++ client/src/__tests__/NewBill.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import { fireEvent, screen } from "@testing-library/dom"
+import { fireEvent, screen, waitFor } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill"
 import { ROUTES_PATH } from "../constants/routes"
@@ -41,249 +41,304 @@ describe("Given I am connected as an employee", () => {
       const mailIcon = screen.getByTestId('icon-mail')
       expect(mailIcon.className).toEqual('active-icon')
     })
+    describe("When I change file with a valid one", () => {
+      test('Then is should be ok', () => {
+        const newBill = new NewBill({ document, onNavigate, store: mockStore, localStorage: window.localStorage })
 
-    test('When I change file with a valid one, should be ok', () => {
-      const newBill = new NewBill({ document, onNavigate, store: mockStore, localStorage: window.localStorage })
+        const handleChangeFile = jest.fn((e) => newBill.handleChangeFile(e))
 
-      const handleChangeFile = jest.fn((e) => newBill.handleChangeFile(e))
+        const fileInput = screen.getByTestId('file')
+        fileInput.addEventListener("change", handleChangeFile)
 
-      const fileInput = screen.getByTestId('file')
-      fileInput.addEventListener("change", handleChangeFile)
+        const file = new File(["test"], "test.png", { type: "image/png" })
+        fireEvent.change(fileInput, {
+          target: {
+            files: [file]
+          }
+        })
 
-      const file = new File(["test"], "test.png", { type: "image/png" })
-      fireEvent.change(fileInput, {
-        target: {
-          files: [file]
+        expect(handleChangeFile).toHaveBeenCalled()
+        expect(handleChangeFile).toBeTruthy()
+        expect(fileInput.files[0]).toBe(file)
+        expect(fileInput.files).toHaveLength(1)
+      })
+    })
+
+    describe("When I insert inputs", () => {
+      test('Then they should be all required', () => {
+        let expenseType = screen.getAllByTestId('expense-type')
+        expect(expenseType[0]).toHaveProperty('required')
+        let datePicker = screen.getAllByTestId('datepicker')
+        expect(datePicker[0]).toHaveProperty('required')
+        let amount = screen.getAllByTestId('amount')
+        expect(amount[0]).toHaveProperty('required')
+        let pct = screen.getAllByTestId('pct')
+        expect(pct[0]).toHaveProperty('required')
+        let file = screen.getAllByTestId('file')
+        expect(file[0]).toHaveProperty('required')
+      })
+    })
+
+    describe("When I let file input empty", () => {
+      test("Then it should be invalide", () => {
+        const html = NewBillUI()
+        document.body.innerHTML = html
+
+        const onNavigate = jest.fn()
+        const localStorage = window.localStorage
+        const newBill = new NewBill({
+          document,
+          onNavigate,
+          store: null,
+          localStorage,
+        })
+
+        const form = screen.getByTestId("form-new-bill")
+
+        newBill.handleSubmitTest = jest.fn(newBill.handleSubmit)
+
+        newBill.handleSubmitTest({
+          preventDefault: () => {}
+        })
+
+        const event = {
+          preventDefault: jest.fn()
         }
-      })
 
-      expect(handleChangeFile).toHaveBeenCalled()
-      expect(handleChangeFile).toBeTruthy()
-      expect(fileInput.files[0]).toBe(file)
-      expect(fileInput.files).toHaveLength(1)
+        const result = newBill.handleSubmit(event)
+
+        expect(newBill.handleSubmitTest).toHaveBeenCalled()
+        expect(result.succes).toBe(false)
+        expect(result.error).toBe('invalide or empty file(s)')
+      })
     })
 
-    // TODO: bellow code don't incrase cover range
-    test('Then All required input are required', () => {
-      let expenseType = screen.getAllByTestId('expense-type')
-      expect(expenseType[0]).toHaveProperty('required')
-      let datePicker = screen.getAllByTestId('datepicker')
-      expect(datePicker[0]).toHaveProperty('required')
-      let amount = screen.getAllByTestId('amount')
-      expect(amount[0]).toHaveProperty('required')
-      let pct = screen.getAllByTestId('pct')
-      expect(pct[0]).toHaveProperty('required')
-      let file = screen.getAllByTestId('file')
-      expect(file[0]).toHaveProperty('required')
+    describe("When I enter an invalide file format", () => {
+      test("Then it should be invalide", () => {
+        const html = NewBillUI()
+        document.body.innerHTML = html
+
+        const onNavigate = jest.fn()
+        const localStorage = window.localStorage
+        const newBill = new NewBill({
+          document,
+          onNavigate,
+          store: null,
+          localStorage,
+        })
+
+        const event = {
+          preventDefault: jest.fn()
+        }
+
+        // Create file with invalide format, like .PDF
+        const invalidFile = new File(['file contents'], 'invalid.pdf', { type: 'application/pdf' })
+
+        const fileInput = screen.getByTestId("file")
+
+        // Trigger change event
+        fireEvent.change(fileInput, { target: { files: [invalidFile] } })
+
+        // Call handleChangeFile manually
+        newBill.handleChangeFile(event)
+
+        // Check if error appened
+        const inputImgMessage = document.querySelector('#inputImgMessage')
+        expect(inputImgMessage.innerHTML).toBe("Seuls les fichiers png, jpeg, jpg et gif sont autorisés.")
+        expect(fileInput.value).toBe('') // be sure that input file is empty
+      })
     })
 
-    test("Test if file is required", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
+    describe("When I do not enter any file", () => {
+      test("Then it should not be valide", () => {
+        const html = NewBillUI()
+        document.body.innerHTML = html
 
-      const onNavigate = jest.fn()
-      const localStorage = window.localStorage
-      const newBill = new NewBill({
-        document,
-        onNavigate,
-        store: null,
-        localStorage,
+        const onNavigate = jest.fn()
+        const localStorage = window.localStorage
+        const newBill = new NewBill({
+          document,
+          onNavigate,
+          store: null,
+          localStorage,
+        })
+
+        const event = {
+          preventDefault: jest.fn()
+        }
+
+        const fileInput = screen.getByTestId("file")
+
+        // Call handleChangeFile manually
+        const result = newBill.handleChangeFile(event)
+
+        // Check result's succes === false
+        expect(result.succes).toBe(false)
+        expect(result.error).toBe("empty file(s)")
       })
-
-      const form = screen.getByTestId("form-new-bill")
-
-      newBill.handleSubmitTest = jest.fn(newBill.handleSubmit)
-
-      newBill.handleSubmitTest({
-        preventDefault: () => {}
-      })
-
-      const event = {
-        preventDefault: jest.fn()
-      };
-
-      const result = newBill.handleSubmit(event);
-
-      expect(newBill.handleSubmitTest).toHaveBeenCalled()
-      expect(result.succes).toBe(false);
-      expect(result.error).toBe('invalide or empty file(s)');
     })
 
-    test("Test if file has an invalid format", () => {
-      const html = NewBillUI();
-      document.body.innerHTML = html;
+    describe("When I enter an invalid format or an empty one", () => {
+      test("Then it should be invalide", () => {
+        const html = NewBillUI()
+        document.body.innerHTML = html
 
-      const onNavigate = jest.fn();
-      const localStorage = window.localStorage;
-      const newBill = new NewBill({
-        document,
-        onNavigate,
-        store: null,
-        localStorage,
-      });
+        const onNavigate = jest.fn()
+        const localStorage = window.localStorage
+        const newBill = new NewBill({
+          document,
+          onNavigate,
+          store: null,
+          localStorage,
+        })
 
-      const event = {
-        preventDefault: jest.fn()
-      };
+        const event = {
+          preventDefault: jest.fn()
+        }
 
-      // Create file with invalide format, like .PDF
-      const invalidFile = new File(['file contents'], 'invalid.pdf', { type: 'application/pdf' });
+        // Create file with invalide format, like .PDF
+        const invalidFile = new File(['file contents'], 'invalid.pdf', { type: 'application/pdf' })
 
-      const fileInput = screen.getByTestId("file");
+        const fileInput = screen.getByTestId("file")
 
-      // Trigger change event
-      fireEvent.change(fileInput, { target: { files: [invalidFile] } });
+        // Use jest.spyOn to simulate empty
+        jest.spyOn(fileInput, 'files', 'get').mockReturnValue([])
 
-      // Call handleChangeFile manually
-      newBill.handleChangeFile(event);
+        // Call handleChangeFile manually
+        const emptyFileResult = newBill.handleSubmit(event)
 
-      // Check if error appened
-      const inputImgMessage = document.querySelector('#inputImgMessage');
-      expect(inputImgMessage.innerHTML).toBe("Seuls les fichiers png, jpeg, jpg et gif sont autorisés.");
-      expect(fileInput.value).toBe(''); // be sure that input file is empty
-    });
+        // Check result's succes === false
+        expect(emptyFileResult.succes).toBe(false)
+        expect(emptyFileResult.error).toBe("invalide or empty file(s)")
 
-    test("Test if no file is selected", () => {
-      const html = NewBillUI();
-      document.body.innerHTML = html;
+        // Reset input file to test invalide format
+        jest.spyOn(fileInput, 'files', 'get').mockReturnValue([invalidFile])
 
-      const onNavigate = jest.fn();
-      const localStorage = window.localStorage;
-      const newBill = new NewBill({
-        document,
-        onNavigate,
-        store: null,
-        localStorage,
-      });
+        // Call handleChangeFile manually
+        const result = newBill.handleSubmit(event)
 
-      const event = {
-        preventDefault: jest.fn()
-      };
+        // Check result's succes === false
+        expect(result.succes).toBe(false)
+        expect(result.error).toBe("invalide or empty file(s)")
 
-      const fileInput = screen.getByTestId("file");
+        // Reset input after testing
+        jest.restoreAllMocks()
+      })
+    })
 
-      // Call handleChangeFile manually
-      const result = newBill.handleChangeFile(event);
+    describe("When I fill everyting correctly", () => {
+      test("Then a new bill should appear on submit", async () => {
+        const html = NewBillUI()
+        document.body.innerHTML = html
 
-      // Check result's succes === false
-      expect(result.succes).toBe(false);
-      expect(result.error).toBe("empty file(s)");
-    });
+        jest.spyOn(mockStore, "bills")
 
-    test("Test if file has an invalid format or is empty", () => {
-      const html = NewBillUI();
-      document.body.innerHTML = html;
+        const onNavigate = jest.fn()
+        const localStorage = window.localStorage
+        const newBill = new NewBill({
+          document,
+          onNavigate,
+          store: mockStore,
+          localStorage,
+        })
 
-      const onNavigate = jest.fn();
-      const localStorage = window.localStorage;
-      const newBill = new NewBill({
-        document,
-        onNavigate,
-        store: null,
-        localStorage,
-      });
+        // Simulate filling form inputs
+        const expenseType = screen.getByTestId("expense-type")
+        expenseType.value = "Transports"
+        expect(expenseType.value).toBe("Transports")
 
-      const event = {
-        preventDefault: jest.fn()
-      };
+        const expenseName = screen.getByTestId("expense-name")
+        expenseName.value = "TGV Paris Tlse"
+        expect(expenseName.value).toBe("TGV Paris Tlse")
 
-      // Create file with invalide format, like .PDF
-      const invalidFile = new File(['file contents'], 'invalid.pdf', { type: 'application/pdf' });
+        const datePicker = screen.getByTestId("datepicker")
+        datePicker.value = "2023-04-27"
+        expect(datePicker.value).toBe("2023-04-27")
 
-      const fileInput = screen.getByTestId("file");
+        const amount = screen.getByTestId("amount")
+        amount.value = "40"
+        expect(amount.value).toBe("40")
 
-      // Use jest.spyOn to simulate empty
-      jest.spyOn(fileInput, 'files', 'get').mockReturnValue([]);
+        const vat = screen.getByTestId("vat")
+        vat.value = "60"
+        expect(vat.value).toBe("60")
 
-      // Call handleChangeFile manually
-      const emptyFileResult = newBill.handleSubmit(event);
+        const pct = screen.getByTestId("pct")
+        pct.value = "20"
+        expect(pct.value).toBe("20")
 
-      // Check result's succes === false
-      expect(emptyFileResult.succes).toBe(false);
-      expect(emptyFileResult.error).toBe("invalide or empty file(s)");
+        const commentary = screen.getByTestId("commentary")
+        commentary.value = "commentary"
+        expect(commentary.value).toBe("commentary")
 
-      // Reset input file to test invalide format
-      jest.spyOn(fileInput, 'files', 'get').mockReturnValue([invalidFile]);
 
-      // Call handleChangeFile manually
-      const result = newBill.handleSubmit(event);
+        const fileInput = screen.getByTestId("file")
+        const file = new File(["file contents"], "valid.png", { type: "image/png" })
 
-      // Check result's succes === false
-      expect(result.succes).toBe(false);
-      expect(result.error).toBe("invalide or empty file(s)");
+        // fireEvent.change(inputEmailUser, { target: { value: "pasunemail" } })
+        fireEvent.change(fileInput, {
+          target: {
+            files: [file],
+          },
+        })
+        expect(fileInput.files[0]).toBe(file)
 
-      // Reset input after testing
-      jest.restoreAllMocks();
-    });
 
-    test("Test bill creation in handleSubmit", async () => {
-      const html = NewBillUI();
-      document.body.innerHTML = html;
+        // jest.spyOn(fileInput, 'files', 'get').mockReturnValue([validFile])
 
-      const onNavigate = jest.fn();
-      const localStorage = window.localStorage;
-      const newBill = new NewBill({
-        document,
-        onNavigate,
-        store: mockStore,
-        localStorage,
-      });
+        // Call handleChangeFile manually
+        const event = {
+          preventDefault: jest.fn(),
+          target: document.querySelector(`form[data-testid="form-new-bill"]`),
+        }
 
-      // Simulate filling form inputs
-      screen.getByTestId("expense-type").value = "Transports";
-      screen.getByTestId("expense-name").value = "TGV Paris Tlse";
-      screen.getByTestId("datepicker").value = "2023-04-27";
-      screen.getByTestId("amount").value = "40";
-      screen.getByTestId("vat").value = "60";
-      screen.getByTestId("pct").value = "20";
-      screen.getByTestId("commentary").value = "commentary";
+        // Check is creation succeed
+        const form = screen.getByTestId("form-new-bill")
+        // screen.debug(screen.getByTestId("form-new-bill"))
 
-      // Create valide file
-      const validFile = new File(['file contents'], 'valid.png', { type: 'image/png' });
-      const fileInput = screen.getByTestId("file");
-      jest.spyOn(fileInput, 'files', 'get').mockReturnValue([validFile]);
+        const handleSubmit = jest.fn((e) => newBill.handleSubmit(e))
 
-      // Call handleChangeFile manually
-      const event = {
-        preventDefault: jest.fn(),
-        target: document.querySelector(`form[data-testid="form-new-bill"]`),
-      };
-      const result = newBill.handleSubmit(event);
+        form.addEventListener("submit", handleSubmit)
+        fireEvent.submit(form)
+        expect(handleSubmit).toHaveBeenCalled()
 
-      // Check is creation succeed
-      expect(result.succes).toBe(true);
-    });
+        const result = newBill.handleSubmit(event)
+        expect(result.succes).toBe(true)
+      })
+    })
 
-    test("Test bill creation with invalid fields", async () => {
-      const html = NewBillUI();
-      document.body.innerHTML = html;
+    describe("When I fill everyting but with an invalide file", () => {
+      test("Then bill creation should fail", async () => {
+        const html = NewBillUI()
+        document.body.innerHTML = html
 
-      const onNavigate = jest.fn();
-      const localStorage = window.localStorage;
-      const newBill = new NewBill({
-        document,
-        onNavigate,
-        store: mockStore,
-        localStorage,
-      });
+        const onNavigate = jest.fn()
+        const localStorage = window.localStorage
+        const newBill = new NewBill({
+          document,
+          onNavigate,
+          store: mockStore,
+          localStorage,
+        })
 
-      // Simulate filling only the first input field (expense-type)
-      screen.getByTestId("expense-type").value = "Transports";
+        // Simulate filling only the first input field (expense-type)
+        screen.getByTestId("expense-type").value = "Transports"
 
-      // Create invalid file
-      const validFile = new File(['file contents'], 'valid.pdf', { type: 'application/pdf' });
-      const fileInput = screen.getByTestId("file");
-      jest.spyOn(fileInput, 'files', 'get').mockReturnValue([validFile]);
+        // Create invalid file
+        const validFile = new File(['file contents'], 'valid.pdf', { type: 'application/pdf' })
+        const fileInput = screen.getByTestId("file")
+        jest.spyOn(fileInput, 'files', 'get').mockReturnValue([validFile])
 
-      // Call handleSubmit manually
-      const event = {
-        preventDefault: jest.fn(),
-        target: document.querySelector(`form[data-testid="form-new-bill"]`),
-      };
-      const result = newBill.handleSubmit(event);
+        // Call handleSubmit manually
+        const event = {
+          preventDefault: jest.fn(),
+          target: document.querySelector(`form[data-testid="form-new-bill"]`),
+        }
+        const result = newBill.handleSubmit(event)
 
-      // Check if creation fails when other fields are empty
-      expect(result.succes).toBe(false);
-    });
+        // Check if creation fails when other fields are empty
+        expect(result.succes).toBe(false)
+      })
+    })
   })
 })
